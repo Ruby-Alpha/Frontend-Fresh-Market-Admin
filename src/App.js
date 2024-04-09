64dc1a3a7be8be5409bd36f661ee3f2508f7a069
fredericka <frederickatettey10@gmail.com>
2024-04-09 22:00:25 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+
+])
+
+
+
+function App() {
+  return (
+    
+      <>
+      
+      <RouterProvider router={router}/>
+      
+      </>
+  
+  );
+}
+
+export default App;
